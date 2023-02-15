542a39886216beb101a260ad2c2c840ec812d43d
vales15 <valeria.sierra77@soy.sena.edu.co>
2023-02-15 11:21:16 -0500

    se agregaron comentario y la operacion de resta

--- ejercicio_suma.py
+++ ejercicio_suma.py
@@ -1,6 +1,18 @@
+
 number_one = 8
 number_two = 5
-
-sumar = number_one + number_two
+#this is a comment of one line
+#operacion de sumar los dos numeros 
+"""
+this is a multi-line comment
+sumar =number_one + number_two
 
 print(sumar)
+"""
+resta= number_one - number_two
+print(resta)
+
+
+
+
+
